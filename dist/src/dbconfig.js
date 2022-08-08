b37146b26c4920dcedc061094fb6f02855c4a266
noisyass2 <bernard.rivera.m@gmail.com>
2022-08-08 12:07:27 +0800

    added logs for connection string

--- dist/src/dbconfig.js
+++ dist/src/dbconfig.js
@@ -30,6 +30,8 @@ dotenv.config();
 const isProduction = process.env.NODE_ENV === 'production';
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 console.log(connectionString);
+console.log(isProduction);
+console.log(process.env.NODE_ENV);
 exports.pool = new pg_1.Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
     ssl: isProduction,
--- src/dbconfig.ts
+++ src/dbconfig.ts
@@ -5,6 +5,8 @@ dotenv.config();
 const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 console.log(connectionString);
+console.log(isProduction);
+console.log(process.env.NODE_ENV);
 export const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   ssl: isProduction,
